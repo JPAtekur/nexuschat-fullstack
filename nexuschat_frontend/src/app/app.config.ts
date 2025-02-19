2094697393cbc2e9ba1abc4abc45e62ca22d069b
Atekur Rahman <atekur.rm@gmail.com>
2025-02-19 23:12:06 +0600

    add main component and chat component

--- nexuschat_frontend/src/app/app.config.ts
+++ nexuschat_frontend/src/app/app.config.ts
@@ -6,14 +6,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import { KeycloakService } from './utils/keycloak/keycloak.service';
+import { keycloakHttpInterceptor } from './utils/http/keycloak-http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
     provideAppInitializer((): Promise<void> => {
       const initFn: () => Promise<void> = ((key: KeycloakService): () => Promise<void> => {
         return (): Promise<void> => key.init();
