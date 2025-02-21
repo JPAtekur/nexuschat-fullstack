ab031d9e3b370399c40b56c696b9b4f46e3c9f97
Atekur Rahman <atekur.rm@gmail.com>
2025-02-21 23:11:03 +0600

    add chat-list components

--- nexuschat_frontend/src/app/components/chat-list/chat-list.component.ts
+++ nexuschat_frontend/src/app/components/chat-list/chat-list.component.ts
@@ -8,7 +8,23 @@ import { ChatResponse } from '../../services/models';
   styleUrl: './chat-list.component.scss'
 })
 export class ChatListComponent {
+  
+  wrapMessage(lastMessage: string | undefined): string {
+    if(lastMessage && lastMessage.length <= 20) {
+      return lastMessage;
+    }
+    return lastMessage?.substring(0, 17) + '...';
+  }
+
+  chatClicked(_t16: ChatResponse) {
+    throw new Error('Method not implemented.');
+  }
+
+  searchContact() {
+    throw new Error('Method not implemented.');
+  }
 
   chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
+  searchNewContact = false;
 
 }
